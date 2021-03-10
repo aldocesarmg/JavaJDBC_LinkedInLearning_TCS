54ae04cfdc10c30cb52dfc9acd8ef4d1e02ae828
aldocesarmg <aldocesarmg@gmail.com>
2021-03-10 12:58:13 -0600

    Consultas con parametros usando PreparedStatement

--- src/com/aldocesarmg/Main.java
+++ src/com/aldocesarmg/Main.java
@@ -6,13 +6,17 @@ import java.sql.*;
 
 public class Main {
 
+    public static final String ConsultaSQL = "SELECT tourId, tourName, price FROM tours WHERE price <= ?";
+
     public static void main(String[] args) throws SQLException {
+
+        ResultSet rs2 = null;
 
         try (   // Toda la información contenida dentro de estos paréntesis se cerrará automáticamente al terminar
                 // esta sentencia de try catch
                 Connection conexion = DBUtil.getConnection(DBType.MYSQL);
                 Statement stmt = conexion.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-                ResultSet rs = stmt.executeQuery("SELECT * FROM tours");
+                ResultSet rs = stmt.executeQuery("SELECT * FROM tours LIMIT 5");
                 ) {
 
             System.out.println("Connected");
@@ -24,6 +28,24 @@ public class Main {
 
         } catch (SQLException throwables) {
             System.err.println(throwables);
+        }
+
+        //NUEVA CONSULTA CON PARÁMETROS
+        System.out.println("\nNueva consulta con parámetros\n");
+
+        try (
+                Connection conexion = DBUtil.getConnection(DBType.MYSQL);
+                PreparedStatement stmt2 = conexion.prepareStatement(ConsultaSQL, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                ) {
+
+            stmt2.setDouble(1, 500);
+            rs2 = stmt2.executeQuery();
+            Tours.displayData(rs2);
+
+        } catch (SQLException throwables) {
+
+        } finally {
+            if (rs2 != null) rs2.close();
         }
     }
 }
