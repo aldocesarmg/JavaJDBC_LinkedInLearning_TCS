46fef12a6ee9501e8939d83728b6db7ff52f4c90
aldocesarmg <aldocesarmg@gmail.com>
2021-03-10 11:44:24 -0600

    Cerrado automatico de los recursos de la consulta al colocarlos dentro de los parentesis del try catch

--- src/com/aldocesarmg/Main.java
+++ src/com/aldocesarmg/Main.java
@@ -5,32 +5,21 @@ import java.sql.*;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        Connection conexion = null;
-        Statement stmt = null;
-        ResultSet rs = null;
 
-        try {
-            conexion = DBUtil.getConnection(DBType.MYSQL);
+        try (   // Toda la información contenida dentro de estos paréntesis se cerrará automáticamente al terminar
+                // esta sentencia de try catch
+                Connection conexion = DBUtil.getConnection(DBType.MYSQL);
+                Statement stmt = conexion.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                ResultSet rs = stmt.executeQuery("SELECT * FROM states");
+                ) {
+
             System.out.println("Connected");
-
-            stmt = conexion.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            rs = stmt.executeQuery("SELECT * FROM states");
 
             rs.last();
             System.out.println("Number of rows: " + rs.getRow());
 
         } catch (SQLException throwables) {
             System.err.println(throwables);
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (stmt != null) {
-                stmt.close();
-            }
-            if (conexion != null) {
-                conexion.close();
-            }
         }
     }
 }
